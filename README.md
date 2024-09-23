d0c62bfd84a52e11005c1eeb62c1755548247009
iwolfisberg <iris.wolfisberg@edufr.ch>
2024-09-23 17:03:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Quiz - connaissances JavaScript
+
+## Prérequis
+
+- Git installé
+- NPM installé
+
+## Installation
+
+Cloner le repository git
+
+```
+git clone git@github.com:eikon-frontend/starterkit.git <nom du projet>
+```
+
+Se rendre dans le dossier du projet, puis installer les dépendances avec NPM
+
+```
+cd quiz-javascript
+npm install
+```
+
+## Instructions
+
+Lancez le serveur
+
+```
+npm run dev
+```
+
+Rendez vous dans le fichier `src/js/main.js`.
+
+Lisez les instructions et répondez aux questions.
---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+// Quiz: Veuillez répondre aux questions ci-dessous. (50 min) (+15 min pour les personnes bénéficiant de mesures supplémentaires)
+// Lorsqu'il y a des sous-points (questions 7, 8 et 11), ils vous guident dans les diverses étapes de l'exercice.
+// Le quiz est individuel. VOus avez cependant accès à tous les supports de cours, ainsi qu'internet.
+// -----------------------
+
+// 1. Expliquez ce qu'est-ce qu'une variable.
+// REPONSE:
+
+// -----------------------
+// 2. Déclarez une variable s'appelant "firstName" et dont la valeur est votre prénom.
+
+// -----------------------
+// 3. Déclarez une variable s'appelant "age" et dont la valeur est votre âge. Attention, sa valeur pourrait être modifiée par la suite.
+
+// -----------------------
+// 4. Dans le code HTML (lignes 17 - 23), citez les divers éléments composant l'élément HTML <input>:
+// - Balise HTML:
+// - Classe(s):
+// - Attribut(s):
+
+// -----------------------
+// 5. Expliquez la différence entre une class et un ID.
+// REPONSE:
+
+// -----------------------
+// 6. A quoi sert la méthode document.querySelector("..."); ?
+// REPONSE:
+
+// -----------------------
+// 7. A l'aide de la méthode document.querySelector("..."), sélectionnez l'élément <p> (ligne 14 du code HTML):
+// 7.1 Créez une variable appelée "cardContent".
+// 7.2 Assignez lui l'élément <p> (ligne 14 du code HTML) que vous avez sélectionné en 7.1.
+
+// -----------------------
+// 8. Utilisez la variable "cardContent" créée à l'exercice 7:
+// 8.1 Utilisez la propriété .classList pour ajouter (.add("...")) la class ".highlight" à l'élément HTML.
+
+// -----------------------
+// 9. Récupérez la valeur de l'attribut "placeholder" (.getAttribute("...")) de l'élément HTML <input> (ligne 17 du code HTML).
+
+// -----------------------
+// 10. Expliquez ce qu'est un écouteur d'événement.
+// REPONSE:
+
+// -----------------------
+// 11. Sur l'élément HTML button (ligne 26 du code HTML), écoutez l'événement "click":
+// 11.1 : Sélectionnez l'élément HTML <button> (ligne 26 du code HTML).
+// 11.2 : Utilisez la méthode .addEventListener("...", function() {...}) sur l'élément HTML sélectionné en 11.1 pour écouter l'événement "click".
+// 11.3 : Lorsqu'un.e utilisateurice clique sur le bouton, ajoutez/supprimez (toggle) la class ".hide" de l'élément HTML <p> (ligne 29-31 du code HTML).
